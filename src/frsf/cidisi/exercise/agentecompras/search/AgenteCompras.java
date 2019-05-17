feb112e15171dc64cef36f925414b532636ba4d6
Julian Petric <julipetric@gmail.com>
2019-05-17 11:42:09 -0300

    Resuelve ma o meno bien
    Arreglar la acción compra que no se muestra en el arbol. Comparar con moverse y ver.

--- src/frsf/cidisi/exercise/agentecompras/search/AgenteCompras.java
+++ src/frsf/cidisi/exercise/agentecompras/search/AgenteCompras.java
@@ -26,9 +26,39 @@ import frsf.cidisi.faia.solver.search.Search;
 
 public class AgenteCompras extends SearchBasedAgent {
 
-	public AgenteCompras(ArrayList<Integer> listaProductosDeseados, int tipoTransporte, int[][] tiemposOrigen,
-			int[][] distOrigen, int recurso) {
+	public AgenteCompras() {
 
+		//DEFINICION DE PARAMETROS INICIALES PARTICULARES DE UN CASO DE EJECUCION
+    	//Productos deseados: 2, 4 y 5
+    	ArrayList<Integer> listaProductosDeseados = new ArrayList<Integer>();
+    	listaProductosDeseados.add(1);
+    	listaProductosDeseados.add(2);
+    	listaProductosDeseados.add(5);
+    	
+    	//Transporte preferido: indefinido
+    	int tipoTransporte = 0;
+    	
+    	//Matriz con los tiempos que lleva llegar del origen propuesto a cada uno de los destinos 
+    	//en bici, auto y transporte publico
+    	int [][] tiemposOrigen = new int[][] {
+    			{2,	4,	4,	4,	7},
+    			{1,	2,	2,	2,	3},
+    			{2,	4,	4,	4,	6}
+    	};
+    	
+    	//Matriz para las distancias que representa moverse del origen propuesto a cada uno de los destinos 
+    	//en bici, auto y transporte publico
+    	int[][] distOrigen = new int[][] {
+    			{308,	616,	616,	616,	924},
+    			{240,	480,	480,	480,	720},
+    			{420,	840,	840,	840,	1260}
+    	};
+    	
+    	//Definicion recurso a priorizar: costo mínimo total
+    	int recurso = 1;
+    	//FIN DEFINICION PARAMETROS INCIALES
+		
+		
 		// The Agent Goal
 		ObjetivoAgente agGoal = new ObjetivoAgente();
 
--- src/frsf/cidisi/exercise/agentecompras/search/AgenteComprasMain.java
+++ src/frsf/cidisi/exercise/agentecompras/search/AgenteComprasMain.java
@@ -9,38 +9,7 @@ public class AgenteComprasMain {
 
     public static void main(String[] args) throws PrologConnectorException {
         
-    	//DEFINICION DE PARAMETROS INICIALES PARTICULARES DE UN CASO DE EJECUCION
-    	//Productos deseados: 2, 4 y 5
-    	ArrayList<Integer> listaProductosDeseados = new ArrayList<Integer>();
-    	listaProductosDeseados.add(2);
-    	listaProductosDeseados.add(4);
-    	listaProductosDeseados.add(5);
-    	
-    	//Transporte preferido: indefinido
-    	int tipoTransporte = 0;
-    	
-    	//Matriz con los tiempos que lleva llegar del origen propuesto a cada uno de los destinos 
-    	//en bici, auto y transporte publico
-    	int [][] tiemposOrigen = new int[][] {
-    			{2,	4,	4,	4,	7},
-    			{1,	2,	2,	2,	3},
-    			{2,	4,	4,	4,	6}
-    	};
-    	
-    	//Matriz para las distancias que representa moverse del origen propuesto a cada uno de los destinos 
-    	//en bici, auto y transporte publico
-    	int[][] distOrigen = new int[][] {
-    			{308,	616,	616,	616,	924},
-    			{240,	480,	480,	480,	720},
-    			{420,	840,	840,	840,	1260}
-    	};
-    	
-    	//Definicion recurso a priorizar: costo mínimo total
-    	int recurso = 1;
-    	//FIN DEFINICION PARAMETROS INCIALES
-    	
-    	
-    	AgenteCompras agent = new AgenteCompras(listaProductosDeseados, tipoTransporte, tiemposOrigen, distOrigen, recurso);
+    	AgenteCompras agent = new AgenteCompras();
     	
         AmbienteAgenteCompras environment = new AmbienteAgenteCompras();
 
